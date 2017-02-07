d02e12920525f4da7559574d571bb657af43ee5e
(╬☉д⊙) <liuhpleon@gmail.com>
2017-02-07 01:53:52 -0800

    Waiting for add information and add some js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,48 @@
 /* json data*/
-var summery = "I am a student in UCR and I am looking for an internship in summer 2017";
+var summery = " I am a master student in UC Riverside, I received my BS degree"
+      +"in one of top engineering universities in China."
+      +"With the great passion and solid foundation of programming, "
+      +"after I achieve my 1st Master's degree, I start my 2nd "
+      +"Master's degree in Computer Engineering. I am a quick learner and a team "
+      +"worker with strong communication and "
+      +"programming skills, currently I am seeking for a Software "
+      +"Development Engineer internship in summer 2017.";
 
 var contacts = {
-	"cellphone": "111-111-1111",
-	"email": "1234@gmail.com",
-	"linkedin": ["linkedin", "link1"],
-	"github": ["github", "link2"]
+	"cellphone": "979-676-7182",
+	"email": "hliu033@ucr.edu",
+	"linkedin": ["linkedin", "linkedin.com/profile/view?id=AAIAABds9pYBomu4yWh-NNveoMLnWrkObF5t7uo&trk=nav_responsive_tab_profile_pic"],
+	"github": ["github", "https://github.com/liuhpleon"]
 }
 var experiences = [
     {
     	"title":"Front End Intern",
-    	"contribute":"using javascript"
-    },
-    {
-    	"title":"Full stack Intern",
-    	"contribute":"using back end"
+    	"contribute":"1"
     }
 ];
 
 var projects = [
 	{
-		"name": "search engine",
-		"introduction": "amazon search engine"
+		"name": "p1",
+		"introduction": ""
 	},
 	{
-		"name": "data mining",
-		"introduction": "data mining project"
+		"name": "p2",
+		"introduction": ""
+	},
+	{
+		"name": "p3",
+		"introduction": ""
+	}
+	,
+	{
+		"name": "p4",
+		"introduction": ""
+	}
+	,
+	{
+		"name": "p5",
+		"introduction": ""
 	}
 ];
 
@@ -62,31 +79,43 @@ var skills = {
 
 $('#summery_content').append("<p>"+summery+"</p>");
 
-$("#contact_content").append("<p>"+contacts["cellphone"]+"</p>");
-$("#contact_content").append("<p>"+contacts["email"]+"</p>");
-$("#contact_content").append("<p>"+contacts["linkedin"][0]);
-$("#contact_content").append("<p>"+contacts["github"][0]+"</p>");
+$("#contact_content").append("<div class='all_content ac_4'>Cellphone: "+contacts["cellphone"]+"</div>");
+$("#contact_content").append("<div class='all_content ac_4'>Email: "+contacts["email"]+"</span>"+"</div>");
+$("#contact_content").append("<div class='all_content ac_4'>"+"<a href="+contacts["linkedin"][1]+">Linkedin</a>"+"</div>");
+$("#contact_content").append("<div class='all_content ac_4'>"+"<a href="+contacts["github"][1]+">Github</a>"+"</div>");
 
 for(var i=0;i<experiences.length;i++){
-	$("#experience_content").append("<p>"+experiences[i].title+"</p>");
-	$("#experience_content").append("<p>"+experiences[i].contribute+"</p>");
+	$("#experience_content").append("<div class='all_content'>"+"</div>");
 }
 
 for(var i=0;i<projects.length;i++){
-    $('#project_content').append("<p>"+projects[i].name+"</p>");
-    $('#project_content').append("<p>"+projects[i].introduction+"</p>");
+    $('#project_content').append("<div class='all_content ac_1'>"+projects[i].name+"</div>");
+    $('#project_content').append(projects[i].introduction+"</div>");
 }
 
 for(var i=0;i<education['schools'].length;i++){
-	$('#education_content').append("<p>"+education["schools"][i].name+"</p>");
-	$('#education_content').append("<p>"+education["schools"][i].year+"</p>");
-	$('#education_content').append("<p>"+education["schools"][i].major+"</p>");
+	$('#education_content').append("<div class='all_content ac_2'><p>"+education["schools"][i].name+"</p>"
+	+"<p>"+education["schools"][i].year+"</p>"
+	+"<p>"+education["schools"][i].major+"</p></div>");
 }
 
 for(var skill in skills){
-    $('#skills_content').append(skill+": ");
+    $('#skills_content').append("<div id="+skill+" class='all_content ac_3'>"+skill+":");
     for(var i=0;i<skills[skill].length;i++){
-        $('#skills_content').append("<a>"+skills[skill][i]+" "+"</a>");
+        $("#"+skill).append("<div class='h5'>"+skills[skill][i]+"</div>");
     }
-    $('#skills_content').append("<br>");
+    $('#skills_content').append("</div>");
 }
+
+
+$('.container-fluid').hover(function() {
+	/* Stuff to do when the mouse enters the element */
+	$('.h4 display').css({
+		"background":"red",
+	});
+}, function() {
+	/* Stuff to do when the mouse leaves the element */
+	$('.h4 display').css({
+		"background":"blue",
+	});
+});
